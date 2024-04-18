42273801df987716a7e93824ab19b0d8e2766ab3
oG-Ski <g.remendowski@gmail.com>
2024-04-17 22:47:15 -0700

    firebase works

--- src/index.js
+++ src/index.js
@@ -12,7 +12,26 @@ import './css/navigation.css';
 import './css/footer.css';
 import './css/resourcePageStyling.css';
 import './css/howToPageStyling.css';
-import './css/homePageStyling.css'
+import './css/homePageStyling.css';
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAxTPout1zSSOF-A5t-MlRSjF3WyqXYO60",
+  authDomain: "informatics-capstone-reach.firebaseapp.com",
+  projectId: "informatics-capstone-reach",
+  storageBucket: "informatics-capstone-reach.appspot.com",
+  messagingSenderId: "487666988247",
+  appId: "1:487666988247:web:46ff2515bac846f1bfac92"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
